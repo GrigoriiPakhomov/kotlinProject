917e481c502fac0543ebbbfafe21394c549eca7b
GrigoriiPakhomov <pakhomov-86@list.ru>
2025-04-20 22:23:05 +0800

    Lesson 6.1. Test success

--- src/test/kotlin/CalculatorServiceTest.kt
+++ src/test/kotlin/CalculatorServiceTest.kt
@@ -1,9 +1,32 @@
 package test.kotlin
 
-import org.junit.jupiter.api.extension.ExtendWith
+import io.mockk.every
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.slot
+import main.kotlin.Calculator
+import main.kotlin.CalculatorService
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 @ExtendWith(MockKExtension::class)
 class CalculatorServiceTest {
+
+    val slotA = slot<Double>()
+    val slotB = slot<Double>()
+
+    @Test
+    fun `calc service shold a and b`() {
+        val calculator = mockk<Calculator>()
+        every { calculator.add(capture(slotA), capture(slotB)) }returns 5.0
+        val calculatorService = CalculatorService(calculator)
+//Секция вызова
+        val result = calculatorService.executeOperation("add", 2.0, 3.0)
+//        Секция проверки
+        assertEquals(2.0, slotA.captured)
+        assertEquals(3.0, slotB.captured)
+        assertEquals(5.0, result)
+    }
 }
